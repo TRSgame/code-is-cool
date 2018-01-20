2d3d44694e02acf2b9c8f705463e84a9f596de8b
Unknown <tiger120605@gmail.com>
2018-01-20 19:45:33 +0100

    fixe bug

---
+++ main.js
@@ -0,0 +1,9 @@
+const Discord = require('discord.js');
+
+var bot = new Discord.Client();
+
+bot.on('ready',() => {
+   console.log("Bot Ready");   
+}) 
+
+bot.login('NDA0MzI5MjM5NTU5Nzk4Nzg0.DUUQrQ.SdBHe30p1fWW_y_3UAwaKjwiaGc');
